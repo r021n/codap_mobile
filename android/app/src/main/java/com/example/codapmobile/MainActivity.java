f46ac4cf8ed12f6456213a43d7c7f6ab0f54482b
r021n <rozinabdul25@gmail.com>
2026-05-04 15:24:39 +0700

    environment preparation

---
+++ android/app/src/main/java/com/example/codapmobile/MainActivity.java
@@ -0,0 +1,5 @@
+package com.example.codapmobile;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.codapmobile',
+  appName: 'codap-mobile',
+  webDir: 'dist'
+};
+
+export default config;
